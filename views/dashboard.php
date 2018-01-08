9ea649c2e381c024092ca317d68a96918293f0c9
mmakwakw <mandisi.makwakwa@gmail.com>
2018-01-08 18:55:55 +0200

    KasiCare_Demo_Views_Update

---
+++ views/dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge"
+    >
+
+
+    <link rel="stylesheet"
+          href="../main.css"
+    >
+
+    <title>Dashboard</title>
+</head>
+<body class="bodyClass">
+
+<div class="headerClass flexClass">
+
+    Logo
+</div>
+<div class="sectionClass flexClass">
+
+    <div class="signUpDivButtonClass">Sign Up</div>
+    <div class="signInDivButtonClass">Sign In</div>
+</div>
+<div class="footerClass flexClass">Footer Section</div>
+</body>
+</html>
--- views/index.php
+++ views/index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
+
 <html lang="en">
-<head>
+    <head>
 
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    >
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge"
-    >
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        >
+        <meta http-equiv="X-UA-Compatible"
+              content="ie=edge"
+        >
 
 
-    <link rel="stylesheet"
-          href="../main.css"
-    >
+        <link rel="stylesheet"
+              href="../main.css"
+        >
 
-    <title>KasiCare</title>
-</head>
-<body class="bodyClass">
+        <title>KasiCare</title>
+    </head>
+    <body class="bodyClass">
 
-    <div class="headerClass flexClass debugDimenTwoClass">
+        <div class="headerClass flexClass">
 
-        <div>HomeIcon</div>
-        <div>SearchIcon</div>
-    </div>
-    <div class="sectionClass flexClass debugDimenThreeClass">
+            Logo
+        </div>
+        <div class="sectionClass flexClass">
 
-        <div class="signUpDivButtonClass">Sign Up</div>
-        <div class="signInDivButtonClass">Sign In</div>
-    </div>
-    <div class="footerClass flexClass debugDimenFourClass">Footer Section</div>
-</body>
+            <div class="signUpDivButtonClass">Sign Up</div>
+            <div class="signInDivButtonClass">Sign In</div>
+        </div>
+        <div class="footerClass flexClass">Footer Section</div>
+    </body>
 </html>
