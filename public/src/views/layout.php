08911593d5e7062262a399d8dcf9626ec6889ed8
novice <mandisi.makwakwa@gmail.com>
2018-01-11 07:44:34 +0200

    Dashboard_Template_View_Complete

--- public/src/views/layout.php
+++ public/src/views/layout.php
@@ -1,88 +1,108 @@
 <!DOCTYPE html>
 
 <html lang="en">
-    <head>
+<head>
 
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        >
-        <meta http-equiv="X-UA-Compatible"
-              content="ie=edge"
-        >
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge"
+    >
 
-        <link rel="stylesheet"
-              href="../../../resources/libs/font_awesome/css/font-awesome.css"
-        >
+    <link rel="stylesheet"
+          href="../../../resources/libs/font_awesome/css/font-awesome.css"
+    >
 
-        <link rel="stylesheet"
-              href="../../css/main.css"
-        >
+    <link rel="stylesheet"
+          href="../../css/main.css"
+    >
 
-        <title>Dashboard</title>
-    </head>
-    <body class="bodyClass">
+    <title>Layout</title>
+</head>
+<body class="bodyClass">
 
-        <div class="headerClass flexOneOneClass borderBottomCrimsonDivLine">
+<div class="headerClass flexOneOneClass borderBottomCrimsonDivLine">
 
-            <div class="flexOneOneClass alignTopClass alignLeftClass">
+    <div class="flexOneOneClass alignTopClass alignLeftClass">
 
-                <img src="../../../resources/images/qr_example.png"
-                     class="headerImageClass"
-                />
-            </div>
-            <div class="flexOneOneClass">
+        <img src="../../../resources/images/qr_example.png"
+             class="headerImageClass"
+        />
+    </div>
+    <div class="flexOneOneClass">
 
-                <img src="../../../resources/images/kasiCarelog.png"
-                     class="headerImageClass"
-                />
-            </div>
-            <div class="flexOneOneClass alignTopClass alignRightClass">
+        <img src="../../../resources/images/kasiCarelog.png"
+             class="headerImageClass"
+        />
+    </div>
+    <div class="flexOneOneClass alignTopClass alignRightClass">
 
-                <div class="headerSignOutButtonClass">Sign Out</div>
-            </div>
-        </div>
-        <div class="sectionClass flexOneOneClass">
+        <div class="headerSignOutButtonClass">Sign Out</div>
+    </div>
+</div>
+<div class="sectionClass flexOneOneClass">
 
-            <div class="panelDivClass">
+    <div class="panelDivClass">
 
-                <div class="panelTabDivClass borderBottomCrimsonDivLine">
+        <div class="widthAdjFullClass flexFlowRowClass flexOneOneClass borderBottomCrimsonDivLine">
 
-                    <a href="#"
-                       class="propicDivClass flexOneOneClass textDecoNoneClass"
-                    >
+            <a href="#"
+               class="propicDivClass flexOneOneClass textDecoNoneClass"
+            >
 
-                        <i class="fa fa-image fa-4x"></i>
-                    </a>
-                    <div class="propicInfoPanelDivClass flexOneOneClass">
+                <i class="fa fa-image fa-4x"></i>
+            </a>
+            <div class="propicInfoPanelDivClass flexOneOneClass">
 
-                        <a href="#"
-                           class="panelPracticeDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+                <a href="#"
+                   class="panelPracticeDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
 
-                            <i class="fa fa-home fa-lg flexOneOneClass"></i>
-                            <div class="flexOneOneClass alignLeftClass">Practice</div>
-                        </a>
-                        <a href="#"
-                           class="panelLocationDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+                    <i class="fa fa-home fa-lg flexOneOneClass"></i>
+                    <div class="flexOneOneClass alignLeftClass">Practice</div>
+                </a>
+                <a href="#"
+                   class="panelLocationDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
 
-                            <i class="fa fa-map-marker fa-lg flexOneOneClass"></i>
-                            <div class="flexOneOneClass alignLeftClass">Location</div>
-                        </a>
-                    </div>
-                </div>
-                <div class="panelTabDivClass"></div>
-                <div class="panelTabDivClass"></div>
-                <div class="panelTabDivClass"></div>
-                <div class="panelTabDivClass"></div>
-                <div class="panelTabDivClass"></div>
-                <div class="panelTabDivClass"></div>
-            </div>
-            <div class="sectionDivClass">
-
-                <div class="widthAdjFullClass flexOneOneClass">SecOne</div>
-                <div class="sectionMainDivClass widthAdjFullClass flexOneOneClass">SecTwo</div>
-                <div class="widthAdjFullClass flexOneOneClass">SecThree</div>
+                    <i class="fa fa-map-marker fa-lg flexOneOneClass"></i>
+                    <div class="flexOneOneClass alignLeftClass">Location</div>
+                </a>
             </div>
         </div>
-    </body>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-dashboard flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">My Dashboard</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-user flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Profile</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-money flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Consultation Fees</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-cog flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Settings</div>
+        </a>
+        <a class="panelTabDivClass borderBottomCrimsonDivLine">
+
+            <i class="fa fa-pie-chart flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Statistics</div>
+        </a>
+        <div class="flexOneOneClass">Powered By: Social Tech</div>
+    </div>
+    <div class="sectionDivClass">
+
+        <div class="paddBottomSmallClass widthAdjFullClass flexFlowColClass flexOneOneClass borderBottomCrimsonDivLine"></div>
+        <div class="sectionMainDivClass widthAdjFullClass flexOneOneClass borderBottomCrimsonDivLine"></div>
+        <div class="tooltipSectionClass widthAdjFullClass flexOneOneClass"></div>
+    </div>
+</div>
+</body>
 </html>
