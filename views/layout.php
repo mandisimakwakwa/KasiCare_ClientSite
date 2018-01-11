12d48ae0af503747fb9e069b534aca7f7e2bda76
novice <mandisi.makwakwa@gmail.com>
2018-01-11 05:37:10 +0200

    Dashboard_Template_View_Base_Update

--- views/layout.php
+++ views/layout.php
@@ -1,88 +1,88 @@
 <!DOCTYPE html>
 
 <html lang="en">
-<head>
+    <head>
 
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    >
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge"
-    >
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        >
+        <meta http-equiv="X-UA-Compatible"
+              content="ie=edge"
+        >
 
-    <link rel="stylesheet"
-          href="../resources/libs/font_awesome/css/font-awesome.css"
-    >
+        <link rel="stylesheet"
+              href="../resources/libs/font_awesome/css/font-awesome.css"
+        >
 
-    <link rel="stylesheet"
-          href="../main.css"
-    >
+        <link rel="stylesheet"
+              href="../main.css"
+        >
 
-    <title>Dashboard</title>
-</head>
-<body class="bodyClass">
+        <title>Dashboard</title>
+    </head>
+    <body class="bodyClass">
 
-<div class="headerClass flexOneOneClass borderBottomCrimsonDivLine">
+        <div class="headerClass flexOneOneClass borderBottomCrimsonDivLine">
 
-    <div class="flexOneOneClass alignTopClass alignLeftClass">
+            <div class="flexOneOneClass alignTopClass alignLeftClass">
 
-        <img src="../resources/images/qr_example.png"
-             class="headerImageClass"
-        />
-    </div>
-    <div class="flexOneOneClass">
+                <img src="../resources/images/qr_example.png"
+                     class="headerImageClass"
+                />
+            </div>
+            <div class="flexOneOneClass">
 
-        <img src="../resources/images/kasiCarelog.png"
-             class="headerImageClass"
-        />
-    </div>
-    <div class="flexOneOneClass alignTopClass alignRightClass">
+                <img src="../resources/images/kasiCarelog.png"
+                     class="headerImageClass"
+                />
+            </div>
+            <div class="flexOneOneClass alignTopClass alignRightClass">
 
-        <div class="headerSignOutButtonClass">Sign Out</div>
-    </div>
-</div>
-<div class="sectionClass flexOneOneClass">
-
-    <div class="panelDivClass">
-
-        <div class="panelTabDivClass borderBottomCrimsonDivLine">
-
-            <a href="#"
-               class="propicDivClass flexOneOneClass textDecoNoneClass"
-            >
-
-                <i class="fa fa-image fa-4x"></i>
-            </a>
-            <div class="propicInfoPanelDivClass flexOneOneClass">
-
-                <a href="#"
-                   class="panelPracticeDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
-
-                    <i class="fa fa-home fa-lg flexOneOneClass"></i>
-                    <div class="flexOneOneClass alignLeftClass">Practice</div>
-                </a>
-                <a href="#"
-                   class="panelLocationDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
-
-                    <i class="fa fa-map-marker fa-lg flexOneOneClass"></i>
-                    <div class="flexOneOneClass alignLeftClass">Location</div>
-                </a>
+                <div class="headerSignOutButtonClass">Sign Out</div>
             </div>
         </div>
-        <div class="panelTabDivClass"></div>
-        <div class="panelTabDivClass"></div>
-        <div class="panelTabDivClass"></div>
-        <div class="panelTabDivClass"></div>
-        <div class="panelTabDivClass"></div>
-        <div class="panelTabDivClass"></div>
-    </div>
-    <div class="sectionDivClass">
+        <div class="sectionClass flexOneOneClass">
 
-        <div class="widthAdjFullClass flexOneOneClass">SecOne</div>
-        <div class="sectionMainDivClass widthAdjFullClass flexOneOneClass">SecTwo</div>
-        <div class="widthAdjFullClass flexOneOneClass">SecThree</div>
-    </div>
-</div>
-</body>
+            <div class="panelDivClass">
+
+                <div class="panelTabDivClass borderBottomCrimsonDivLine">
+
+                    <a href="#"
+                       class="propicDivClass flexOneOneClass textDecoNoneClass"
+                    >
+
+                        <i class="fa fa-image fa-4x"></i>
+                    </a>
+                    <div class="propicInfoPanelDivClass flexOneOneClass">
+
+                        <a href="#"
+                           class="panelPracticeDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+
+                            <i class="fa fa-home fa-lg flexOneOneClass"></i>
+                            <div class="flexOneOneClass alignLeftClass">Practice</div>
+                        </a>
+                        <a href="#"
+                           class="panelLocationDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+
+                            <i class="fa fa-map-marker fa-lg flexOneOneClass"></i>
+                            <div class="flexOneOneClass alignLeftClass">Location</div>
+                        </a>
+                    </div>
+                </div>
+                <div class="panelTabDivClass"></div>
+                <div class="panelTabDivClass"></div>
+                <div class="panelTabDivClass"></div>
+                <div class="panelTabDivClass"></div>
+                <div class="panelTabDivClass"></div>
+                <div class="panelTabDivClass"></div>
+            </div>
+            <div class="sectionDivClass">
+
+                <div class="widthAdjFullClass flexOneOneClass">SecOne</div>
+                <div class="sectionMainDivClass widthAdjFullClass flexOneOneClass">SecTwo</div>
+                <div class="widthAdjFullClass flexOneOneClass">SecThree</div>
+            </div>
+        </div>
+    </body>
 </html>
