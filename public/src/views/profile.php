9d8cf57f2b41e0a85bd9b3fc33dce8f574abe45b
novice <mandisi.makwakwa@gmail.com>
2018-01-12 12:45:39 +0200

    Profile_View_Update

---
+++ public/src/views/profile.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge"
+    >
+
+    <link rel="stylesheet"
+          href="../../../resources/libs/font_awesome/css/font-awesome.css"
+    >
+
+    <link rel="stylesheet"
+          href="../../css/main.css"
+    >
+
+    <title>Profile</title>
+</head>
+<body class="bodyClass">
+
+<div class="headerClass flexOneOneClass borderBottomCrimsonDivLine">
+
+    <div class="flexOneOneClass alignTopClass alignLeftClass">
+
+        <img src="../../../resources/images/qr_example.png"
+             class="headerImageClass"
+        />
+    </div>
+    <div class="flexOneOneClass">
+
+        <img src="../../../resources/images/kasiCarelog.png"
+             class="headerImageClass"
+        />
+    </div>
+    <div class="flexOneOneClass alignTopClass alignRightClass">
+
+        <div class="headerSignOutButtonClass">Sign Out</div>
+    </div>
+</div>
+<div class="sectionClass flexOneOneClass">
+
+    <div class="panelDivClass">
+
+        <div class="widthAdjFullClass flexFlowRowClass flexOneOneClass borderBottomCrimsonDivLine">
+
+            <a href="#"
+               class="propicDivClass flexOneOneClass textDecoNoneClass"
+            >
+
+                <i class="fa fa-image fa-4x"></i>
+            </a>
+            <div class="propicInfoPanelDivClass flexOneOneClass">
+
+                <a href="#"
+                   class="panelPracticeDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+
+                    <i class="fa fa-home fa-lg flexOneOneClass"></i>
+                    <div class="flexOneOneClass alignLeftClass">Practice</div>
+                </a>
+                <a href="#"
+                   class="panelLocationDivClass flexOneOneClass textDecoNoneClass cursorPointerToggleClass">
+
+                    <i class="fa fa-map-marker fa-lg flexOneOneClass"></i>
+                    <div class="flexOneOneClass alignLeftClass">Location</div>
+                </a>
+            </div>
+        </div>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-dashboard flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">My Dashboard</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-user flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Profile</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-money flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Consultation Fees</div>
+        </a>
+        <a class="panelTabDivClass">
+
+            <i class="fa fa-cog flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Settings</div>
+        </a>
+        <a class="panelTabDivClass borderBottomCrimsonDivLine">
+
+            <i class="fa fa-pie-chart flexOneOneClass"></i>
+            <div class="flexOneOneClass alignLeftClass">Statistics</div>
+        </a>
+        <div class="flexOneOneClass">Powered By: Social Tech</div>
+    </div>
+    <div class="sectionDivClass">
+
+        <div class="dashboardSectionDivClass paddBottomSmallClass widthAdjFullClass flexFlowColClass flexOneOneClass borderBottomCrimsonDivLine">
+            Profile
+        </div>
+        <div class="sectionMainDivClass widthAdjFullClass flexOneOneClass borderBottomCrimsonDivLine">
+
+            <div class="flexOneOneClass widthAdjFullClass">
+
+                <i class="fa fa-info fa-5x flexOneOneClass borderRightCrimsonDivLine"></i>
+                <div class="profileInfoDivClass flexOneOneClass"></div>
+            </div>
+            <div class="flexOneOneClass widthAdjFullClass">
+
+                <i class="fa fa-map fa-5x flexOneOneClass borderRightCrimsonDivLine"></i>
+                <div class="profileInfoDivClass flexOneOneClass"></div>
+            </div>
+            <div class="flexOneOneClass widthAdjFullClass">
+
+                <i class="fa fa-medkit fa-5x flexOneOneClass borderRightCrimsonDivLine"></i>
+                <div class="profileInfoDivClass flexOneOneClass"></div>
+            </div>
+        </div>
+        <div class="tooltipSectionClass widthAdjFullClass flexOneOneClass"></div>
+    </div>
+</div>
+</body>
+</html>
