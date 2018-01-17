b53d7540968fe7609207a17b56a4e5426ae1d8d2
novice <mandisi.makwakwa@gmail.com>
2018-01-17 09:18:56 +0200

    SignIn - Complete

--- public/src/php/views/signIn.php
+++ public/src/php/views/signIn.php
@@ -39,5 +39,56 @@
     </head>
 
     <body>
+
+        <div class="sectionDiv">
+
+            <div class="flexify parallxFX_Pg bkgdImage_a fulWidth_strch flexFlow_column">
+
+                <div class="flexify fulWidth_strch themeGrey_BkgdColrC">
+
+                    <img src="../../../../resources/images/misc/kasicare.png"
+                         class="logoImg flx_1_1"
+                    />
+                    <div class="flexify flx_8_1 titleTxt_FX fontSize_Larger themeBlack_TxtColrB"></div>
+                    <div class="btn_Small themeBlack_BkgdColrC algnTop">Sign Up</div>
+                </div>
+
+                <div class="containerNarrow themeGrey_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
+
+                    <h1>Admin</h1>
+                </div>
+
+                <form action="#"
+                      method="post"
+                      class="flexify flexFlow_column"
+                >
+
+                    <div class="flexify flx_1_1 flexFlow_row">
+
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="signIn"
+                               id="signIn"
+                               type="text"
+                               placeholder="Please Enter Admin Username or Email"
+                        />
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="password"
+                               id="password"
+                               type="password"
+                               placeholder="Please Enter Password"
+                        />
+                    </div>
+                    <div class="btn_Medium themeGrey_BkgdColrC margin_20">Sign In</div>
+                </form>
+
+                <div class="containerNarrow themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
+
+                    <ul class="fontSize_Small">
+
+                        <li>Required for application functionality</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
