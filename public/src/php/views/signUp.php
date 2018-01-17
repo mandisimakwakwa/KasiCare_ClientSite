eed2ff985d7f52d702c9fae5ee27fa74da137b43
novice <mandisi.makwakwa@gmail.com>
2018-01-17 09:04:09 +0200

    SignUp - Complete

--- public/src/php/views/signUp.php
+++ public/src/php/views/signUp.php
@@ -49,70 +49,67 @@
                     <img src="../../../../resources/images/misc/kasicare.png"
                          class="logoImg flx_1_1"
                     />
-                    <div class="flexify flx_8_1 titleTxt_FX fontSize_Larger themeBlack_TxtColrB">
-
-                        <h1>Sign Up Form</h1>
-                    </div>
+                    <div class="flexify flx_8_1 titleTxt_FX fontSize_Larger themeBlack_TxtColrB"></div>
+                    <div class="flx_1_1"></div>
                 </div>
 
-                <div class="containerNarrow themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
+                <div class="containerNarrow fontSize_Medium themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
 
 
                     <div><h1>Practice Infomation</h1></div>
                 </div>
-                <div class="flexify flx_4_1 flexFlow_row">
 
-                    <input class="flexify flx_4_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="practiceName"
-                           id="practiceName"
-                           type="text"
-                           placeholder="Please Enter Practice Name..."
-                    />
-                    <input class="flexify flx_4_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="geolocation"
-                           id="geolocation"
-                           type="text"
-                           placeholder="Please Enter Longitude - Latitude"
-                    />
+                <form action="#"
+                      method="post"
+                      class="flexify flexFlow_column"
+                >
 
-                    <p class="flexify flx_1_1 fontSize_Small themeBlack_TxtColrC">or</p>
+                    <div class="flexify flx_4_1 flexFlow_row">
 
-                    <input class="flexify flx_4_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="address"
-                           id="address"
-                           type="text"
-                           placeholder="Please Enter Address"
-                    />
-                </div>
-                <div class="flexify flx_1_1 flexFlow_row">
+                        <input class="flexify flx_4_1 themeBlack_BkgdColrC quartrWidth_strch margin_10 padding_10"
+                               name="practiceName"
+                               id="practiceName"
+                               type="text"
+                               placeholder="Please Enter Practice Name..."
+                        />
+                        <input class="flexify flx_8_1 themeBlack_BkgdColrC quartrWidth_strch margin_10 padding_10"
+                               name="geolocation"
+                               id="geolocation"
+                               type="text"
+                               placeholder="Please Enter Longitude - Latitude or Address"
+                        />
+                    </div>
+                    <div class="flexify flx_1_1 flexFlow_row">
 
-                    <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="practice_speciality"
-                           id="practice_speciality"
-                           type="text"
-                           placeholder="Please Enter Practice Speciality"
-                    />
-                    <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="practice_services"
-                           id="practice_services"
-                           type="text"
-                           placeholder="Please Enter Practice Services"
-                    />
-                    <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="office_hours"
-                           id="office_hours"
-                           type="text"
-                           placeholder="Please Enter Office Hours eg, 09H00-13h00"
-                    />
-                </div>
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="practice_speciality"
+                               id="practice_speciality"
+                               type="text"
+                               placeholder="Please Enter Practice Speciality"
+                        />
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="practice_services"
+                               id="practice_services"
+                               type="text"
+                               placeholder="Please Enter Practice Services"
+                        />
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="office_hours"
+                               id="office_hours"
+                               type="text"
+                               placeholder="Please Enter Office Hours eg, 09H00-13h00"
+                        />
+                    </div>
+                </form>
 
                 <div class="containerNarrow themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
 
                     <ul class="fontSize_Small">
 
-                        <li>Needed for application functionality</li>
+                        <li>Required for application functionality</li>
                         <li>Please use comma to seperate services</li>
                     </ul>
+                    <div class="fa fa-angle-double-down btn_SmallCircle"></div>
                 </div>
             </div>
             <div class="flexify parallxFX_Pg bkgdImage_c fulWidth_strch flexFlow_column">
@@ -127,35 +124,56 @@
 
                 <div class="containerNarrow themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
 
-
-                    <div><h1>Profile Infomation</h1></div>
+                    <h1>Admin Profile</h1>
                 </div>
-                <div class="flexify flx_4_1 flexFlow_row">
 
-                    <a class="fa fa-image fa-5x"
-                       href="#"
-                    >
-                    </a>
-                </div>
-                <div class="flexify flx_1_1 flexFlow_row">
+                <form action="#"
+                      method="post"
+                      class="flexify flexFlow_column"
+                >
 
-                    <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
-                           name="practice_speciality"
-                           id="practice_speciality"
-                           type="text"
-                           placeholder="Please Enter Practice Speciality"
-                    />
-                </div>
+                    <div class="flexify flx_1_1 flexFlow_row">
+
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="username"
+                               id="username"
+                               type="text"
+                               placeholder="Please Enter Admin Username"
+                        />
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="email"
+                               id="email"
+                               type="text"
+                               placeholder="Please Enter Email"
+                        />
+                    </div>
+                    <div class="flexify flx_1_1 flexFlow_row">
+
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="password"
+                               id="password"
+                               type="password"
+                               placeholder="Please Enter Password"
+                        />
+                        <input class="flexify flx_1_1 themeBlack_BkgdColrC quartrWidth_strch margin_5 padding_10"
+                               name="passwordCheck"
+                               id="passwordCheck"
+                               type="password"
+                               placeholder="Please Re-Enter Password"
+                        />
+                    </div>
+
+                    <div class="btn_Medium themeDPink_BkgdColrB">Submit</div>
+                </form>
 
                 <div class="containerNarrow themeBlack_TxtColrB flexify flx_1_1 flexFlow_column fulWidth_strch">
 
                     <ul class="fontSize_Small">
 
-                        <li>Needed for application functionality</li>
+                        <li>Required for application functionality</li>
                     </ul>
                 </div>
             </div>
-            <div class="flexify parallxFX_Pg bkgdImage_a fulWidth_strch flexFlow_column"></div>
         </div>
     </body>
 </html>
