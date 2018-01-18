7298e946e22b1331a42fb8f97b31165542b6b096
novice <mandisi.makwakwa@gmail.com>
2018-01-18 05:40:00 +0200

    Statistics Complete

--- public/src/php/views/statistics.php
+++ public/src/php/views/statistics.php
@@ -36,11 +36,11 @@
               href="../../css/debug.css"
         >
 
-        <title>Profile</title>
+        <title>Statistics</title>
     </head>
     <body>
 
-        <div class="sectionDiv fulWidth_strch parallxFX_Pg bkgdImage_c">
+        <div class="sectionDiv fulWidth_strch parallxFX_Pg bkgdImage_a">
 
             <div class="flexify flx_1_1 flexFlow_row fulWidth_strch themeGrey_BkgdColrC">
 
@@ -53,33 +53,35 @@
             <div class="flexify flx_1_1 flexFlow_row fulWidth_strch">
 
                 <div class="fa fa-bars fa-2x btn_MedCircle"></div>
-                <h1 class="fulWidth_strch">Profile</h1>
+                <h1 class="fulWidth_strch">Statistics</h1>
                 <div class="margin_20 padding_5"></div>
             </div>
 
             <div class="flexify flx_1_1 fulWidth_strch themeGrey_TxtColrB">
 
-                <div class="flx_4_1 fontSize_Large flexify flexFlow_column">
-
-                    <a href="#" class="txtDeco_None margin_10 themeGrey_TxtColrB">Practice Name<div class="themeGrey_BkgdColrC margin_5 padding_5 fa fa-pencil fa-1x"></div></a>
-                    <a href="#" class="txtDeco_None margin_10 themeGrey_TxtColrB">Location<div class="themeGrey_BkgdColrC margin_5 padding_5 fa fa-pencil fa-1x"></div></a>
+                <div class="fa fa-user-circle fa-5x margin_10 flx_1_1"></div>
+                <div class="flexify flexFlow_column flx_4_1 fulWidth_strch">
+                    <p>Clients: 33</p>
+                    <p>Barcode Scans: 5</p>
                 </div>
-                <div class="fa fa-image fa-5x margin_10 sCard flx_1_1"></div>
-                <div class="flx_4_1">
-                    <div>Rating : 3.8/5</div>
-                    <a href="#" class="txtDeco_None margin_10 themeGrey_TxtColrB">Mon - Fri: 08H00 - 16H00<div class="themeGrey_BkgdColrC margin_5 padding_5 fa fa-pencil fa-1x"></div></a>
-                    <div class="margin_10"></div>
+                <div class="fa fa-pie-chart fa-5x margin_10 flx_1_1"></div>
+                <div class="flexify flexFlow_column flx_4_1 fulWidth_strch">
+                    <p>Unique Views Today: 57</p>
+                    <p>Five Stars This Week: 2</p>
                 </div>
             </div>
-            <div class="themeBlack_BkgdColrC flexify flx_1_1 fulWidth_strch">
+            <div class="flexify flx_1_1 fulWidth_strch themeBlack_BkgdColrC themeGrey_TxtColrB flexFlow_column">
 
-                    <div class="flexify flx_1_1 themeGrey_TxtColrB flexFlow_column">
+                <div class="fa-2x margin_10 flx_1_1">User Reviews</div>
+                <div class="margin_10 flx_1_1">
 
-                        <div class="fontSize_Medium">Map</div>
-                        <img src="../../../../resources/images/misc/map.png"
-                             class="mapImage"
-                        />
+                    <p>"A bit of a learning curve but well worth it."</p>
+                    <div class="flexify flexFlow_row">
+
+                        <div class="quartrWidth_strch"></div>
+                        <div>4/5</div>
                     </div>
+                </div>
             </div>
         </div>
     </body>
